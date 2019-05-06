0abeaf5f8677977f74efc4d0bbddab0eeeb71337
ych94 <313980657@qq.com>
2019-05-06 11:20:37 +0800

    公告功能的mock文件

---
+++ src/mock/notice.js
@@ -0,0 +1,92 @@
+import Mock from 'mockjs'
+import { param2Obj } from '@/utils'
+
+let List = []
+const count =100
+
+for (let i = 0; i < count; i++) {
+  List.push(Mock.mock({
+    id: Mock.Random.guid(),
+    num:'@increment()',
+    title: Mock.Random.ctitle(),
+    content: Mock.mock('@cparagraph'),
+    createDate: Mock.mock('@datetime("yyyy-MM-dd-HH:mm")')
+  }))
+}
+
+export default {
+  getNoticeList: config => {
+    const { title, page = 1, limit = 20 } = param2Obj(config.url)
+
+    const mockList = List.filter(notice => {
+      if (title && notice.title.indexOf(title) === -1) return false
+      return true
+    })
+
+    const pageList = mockList.filter((item, index) => index < limit * page && index >= limit * (page - 1))
+
+    return {
+      code: 0,
+      data: {
+        total: mockList.length,
+        notices: pageList
+      }
+    }
+  },
+  createNotice: config => {
+    const { id,num, title, content, createDate } = param2Obj(config.url)
+    console.log('66')
+    List.unshift({
+      id: id,
+      num:num,
+      title: title,
+      content: content.replace(/\n/g,"<br/>"),
+      createDate: createDate,
+    })
+    return {
+      code: 0,
+      data: {
+        message: '发布成功'
+      }
+    }
+  },
+  deleteNotice: config => {
+    const { id } = param2Obj(config.url)
+    List = List.filter(u => u.id !== id)
+    return {
+      code: 0,
+      data: {
+        message: '删除成功'
+      }
+    }
+  },
+  batchremove: config => {
+    let { ids } = param2Obj(config.url)
+    ids = ids.split(',')
+    List = List.filter(u => !ids.includes(u.id))
+    return {
+      code: 0,
+      data: {
+        message: '批量删除成功'
+      }
+    }
+  },
+  updateNotice: config => {
+    const { id,num, title, content, createDate} = param2Obj(config.url)
+    List.some(u => {
+      if (u.id === id) {
+        u.num=num
+        u.title = title
+        u.content = content.replace(/\n/g,"<br/>")
+        u.createDate = createDate
+        return true
+      }
+    })
+    return {
+      code: 0,
+      data: {
+        message: '编辑成功'
+      }
+    }
+  }
+}

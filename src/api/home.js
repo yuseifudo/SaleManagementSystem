de15f0c9fe202d10b71025d82e30d1e93091ef36
cny <123456>
2019-05-08 16:55:20 +0800

    首页版本1.2

--- src/api/home.js
+++ src/api/home.js
@@ -4,4 +4,9 @@ const base = ''
 
 export const getHomeData = params => { return axios.get(`${base}/home/data`, { params: params }) }
 
+export const getLchartData = params => { return axios.get(`${base}/home/lchartdata`, { params: params }) }
+
+export const getRchartData = params => { return axios.get(`${base}/home/rchartdata`, { params: params }) }
+
+
 
--- src/components/homechart/homerchart.vue
+++ src/components/homechart/homerchart.vue
@@ -4,6 +4,7 @@
 
 <script>
     import echarts from 'echarts'
+    import { getRchartData } from '@/api/homeTable'
     export default {
       props: {
         className: {
@@ -26,121 +27,141 @@
       data() {
         return {
           chart:null,
+          rChartData:[],
+          nameData:[],
+          cardData:[],
+          cardDataT:[],
+          cardDataB:[]
         }
       },
       mounted() {
-        //初始化echarts图表实例
-        this.chart = echarts.init(document.getElementById("id2"));
-        const option = {
-          title : {
-            text: '全国热销套餐',
-            subtext: '单位：百万',
-            x:'center'
-          },
-          tooltip: {
-            trigger: 'item',
-            formatter: "{a} <br/>{b}: {c} ({d}%)"
-          },
-          toolbox: {
-            feature: {
-              dataView: {show: true, readOnly: false},
-              // magicType: {show: true, type: ['pie','funnel']},
-              restore: {show: true},
-              saveAsImage: {show: true}
+        this.getRchartData();
+      },
+      methods: {
+        getRchartData() {
+          getRchartData().then(res => {
+            var rChartData = res.data.rChartData
+            var obj = eval(rChartData);//转换为JSON对象
+            console.log(obj)
+            for (var i = 0; i < obj.length; i++) {
+                this.cardData.push(obj[i].data[0])
+                // this.cardData.push({value:obj[i].value,name:obj[i].name})
             }
-          },
-          legend: {
-            orient: 'horizontal',
-            x: 'center',
-            y: 'bottom',
-            data:['腾讯王卡','蚂蚁宝卡','京东强卡','工行E卡','百度圣卡','滴滴橙卡']
-          },
-          series: [
-            {
-              name:'售出占比',
-              type:'pie',
-              selectedMode: 'single',
-              radius: [0, '30%'],
+            this.cardDataT=this.cardData.slice(0,3)
+            this.cardDataB=this.cardData.slice(3,6)
+            console.log(this.cardDataT)
+            console.log(this.cardDataB)
+            //初始化echarts图表实例
+            this.chart = echarts.init(document.getElementById("id2"));
+            const option = {
+              title: {
+                text: '全国热销套餐',
+                subtext: '单位：百万',
+                x: 'center'
+              },
+              tooltip: {
+                trigger: 'item',
+                formatter: "{a} <br/>{b}: {c} ({d}%)"
+              },
+              toolbox: {
+                feature: {
+                  dataView: {show: true, readOnly: false},
+                  // magicType: {show: true, type: ['pie','funnel']},
+                  restore: {show: true},
+                  saveAsImage: {show: true}
+                }
+              },
+              legend: {
+                orient: 'horizontal',
+                x: 'center',
+                y: 'bottom',
+                data: ['腾讯王卡', '蚂蚁宝卡', '京东强卡', '工行E卡', '百度圣卡', '滴滴橙卡']
+              },
+              series: [
+                {
+                  name: '售出占比',
+                  type: 'pie',
+                  selectedMode: 'single',
+                  radius: [0, '30%'],
 
-              label: {
-                normal: {
-                  position: 'inner'
-                }
-              },
-              labelLine: {
-                normal: {
-                  show: false
-                }
-              },
-              data:[
-                {value:335, name:'腾讯王卡', selected:true},
-                {value:679, name:'蚂蚁宝卡'},
-                {value:1548, name:'京东强卡'}
-              ]
-            },
-            {
-              name:'售出占比',
-              type:'pie',
-              radius: ['40%', '55%'],
-              label: {
-                normal: {
-                  formatter: '{a|{a}}{abg|}\n{hr|}\n  {b|{b}：}{c}  {per|{d}%}  ',
-                  backgroundColor: '#eee',
-                  borderColor: '#aaa',
-                  borderWidth: 1,
-                  borderRadius: 4,
-                  shadowBlur:3,
-                  shadowOffsetX: 2,
-                  shadowOffsetY: 2,
-                  shadowColor: '#999',
-                  padding: [0, 7],
-                  rich: {
-                    a: {
-                      color: '#999',
-                      lineHeight: 22,
-                      align: 'center'
-                    },
-                    // abg: {
-                    //     backgroundColor: '#333',
-                    //     width: '100%',
-                    //     align: 'right',
-                    //     height: 22,
-                    //     borderRadius: [4, 4, 0, 0]
-                    // },
-                    hr: {
-                      borderColor: '#aaa',
-                      width: '100%',
-                      borderWidth: 0.5,
-                      height: 0
-                    },
-                    b: {
-                      fontSize: 16,
-                      lineHeight: 33
-                    },
-                    per: {
-                      color: '#eee',
-                      backgroundColor: '#334455',
-                      padding: [2, 4],
-                      borderRadius: 2
+                  label: {
+                    normal: {
+                      position: 'inner'
                     }
-                  }
+                  },
+                  labelLine: {
+                    normal: {
+                      show: false
+                    }
+                  },
+                  data:this.cardDataT
+                  // [
+                  // {value: 335, name: '腾讯王卡'},
+                  // {value: 679, name: '蚂蚁宝卡'},
+                  // {value: 1548, name: '京东强卡'}
+                  // ]
+                },
+                {
+                  name: '售出占比',
+                  type: 'pie',
+                  radius: ['40%', '55%'],
+                  label: {
+                    normal: {
+                      formatter: '{a|{a}}{abg|}\n{hr|}\n  {b|{b}：}{c}  {per|{d}%}  ',
+                      backgroundColor: '#eee',
+                      borderColor: '#aaa',
+                      borderWidth: 1,
+                      borderRadius: 4,
+                      shadowBlur: 3,
+                      shadowOffsetX: 2,
+                      shadowOffsetY: 2,
+                      shadowColor: '#999',
+                      padding: [0, 7],
+                      rich: {
+                        a: {
+                          color: '#999',
+                          lineHeight: 22,
+                          align: 'center'
+                        },
+                        // abg: {
+                        //     backgroundColor: '#333',
+                        //     width: '100%',
+                        //     align: 'right',
+                        //     height: 22,
+                        //     borderRadius: [4, 4, 0, 0]
+                        // },
+                        hr: {
+                          borderColor: '#aaa',
+                          width: '100%',
+                          borderWidth: 0.5,
+                          height: 0
+                        },
+                        b: {
+                          fontSize: 16,
+                          lineHeight: 33
+                        },
+                        per: {
+                          color: '#eee',
+                          backgroundColor: '#334455',
+                          padding: [2, 4],
+                          borderRadius: 2
+                        }
+                      }
+                    }
+                  },
+                  data:this.cardDataB
+                  // [
+                  // {value: 335, name: '工行E卡'},
+                  // {value: 310, name: '百度圣卡'},
+                  // {value: 234, name: '滴滴橙卡'},
+                  // ]
                 }
-              },
-              data:[
-                {value:335, name:'工行E卡'},
-                {value:310, name:'百度圣卡'},
-                {value:234, name:'滴滴橙卡'},
-                // {value:135, name:'阿里钉钉卡'},
-                // {value:1048, name:'百度'},
-                // {value:251, name:'谷歌'},
-                // {value:147, name:'必应'},
-                // {value:102, name:'其他'}
               ]
-            }
-          ]
-        };
-        //完成图表绘制的配置
-        this.chart.setOption(option);
+            };
+            //完成图表绘制的配置
+            this.chart.setOption(option);
+          });
+        }
       }
     }
 </script>

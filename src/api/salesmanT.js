6735340da6b4f5c56dea10b50f1ab562c60aa99c
wangz293 <1044749004@qq.com>
2019-05-08 09:45:57 +0800

    Merge branch 'master' of https://github.com/shilongcheng10/SaleManagementSystem
    # Conflicts:
    #	src/router/index.js

---
+++ src/api/salesmanT.js
@@ -0,0 +1,42 @@
+//功能：销售人员列表对应的api文件
+//作者：李娜容
+// 日期：2019/5/6
+
+import request from '@/utils/request'
+
+
+export function removeSalesman(params) {
+  return request({
+    url: '/salesman/remove',
+    method: 'get',
+    params: params
+  })
+}
+export function getSalesmanListPage(params) {
+  return request({
+    url: '/salesman/listpage',
+    method: 'get',
+    params: params
+  })
+}
+export function batchRemoveSalesman(params) {
+  return request({
+    url: '/salesman/batchremove',
+    method: 'get',
+    params: params
+  })
+}
+export function editSalesman(params) {
+  return request({
+    url: '/salesman/edit',
+    method: 'get',
+    params: params
+  })
+}
+export function addSalesman(params) {
+  return request({
+    url: '/salesman/add',
+    method: 'get',
+    params: params
+  })
+}
